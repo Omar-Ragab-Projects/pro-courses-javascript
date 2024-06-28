d286a1b9b0bb0db34b993911b9d5f0d52d1c4ace
Omar Ragab <158287908+Omar-Ragab-Projects@users.noreply.github.com>
2024-06-29 02:09:52 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,14 +43,14 @@ randomSet[0].onclick = function () {
   specialImage.forEach((i) => i.classList.remove("active"));
   randbg = setInterval(() => {
     let randomImg = Math.floor(Math.random() * imags.length);
-    landpage.style.backgroundImage = `url("../imgs/${imags[randomImg]}")`;
+    landpage.style.backgroundImage = `url("./../imgs/${imags[randomImg]}")`;
   }, 10000);
 };
 randomSet[1].onclick = function () {
   this.classList.add("active");
   randomSet[0].classList.remove("active");
   localStorage.setItem("randomBg", "no");
-  landpage.style.backgroundImage = "url(../imgs/bg2.jpg)";
+  landpage.style.backgroundImage = "url(./../imgs/bg2.jpg)";
   localStorage.removeItem("special-image");
   specialImage.forEach((i) => i.classList.remove("active"));
   clearInterval(randbg);
@@ -60,7 +60,7 @@ let landpage = document.querySelector(".landing-page");
 let imags = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg"];
 randbg = setInterval(() => {
   let randomImg = Math.floor(Math.random() * imags.length);
-  landpage.style.backgroundImage = `url("../imgs/${imags[randomImg]}")`;
+  landpage.style.backgroundImage = `url("./../imgs/${imags[randomImg]}")`;
 }, 10000);
 // Local Storage Random Bg
 if (localStorage.getItem("randomBg")) {
@@ -106,74 +106,7 @@ if (localStorage.getItem("special-image")) {
     }
   });
 }
-// Statics Interval
-let staticSection = document.querySelector(".statics");
-let staticNums = document.querySelectorAll(".static-box .information .number");
-let staticPlus = document.querySelectorAll(
-  ".static-box .information .number + span"
-);
-let startedGo = true;
-window.addEventListener("scroll", (e) => {
-  if (window.scrollY >= staticSection.offsetTop - 800) {
-    if (startedGo) {
-      staticNums.forEach((num) => {
-        setTimeout(() => {
-          staticPlus.forEach((p) => (p.style.opacity = "1"));
-        }, 2000);
-        let staticInt = setInterval(() => {
-          num.innerHTML++;
-          if (num.innerHTML === num.dataset.value) {
-            clearInterval(staticInt);
-          }
-        }, 2000 / num.dataset.value);
-      });
-    }
-    startedGo = false;
-  }
-});
-// Categories Images
-let categoriesImgs = document.querySelectorAll(
-  ".categories-section .categories img"
-);
-categoriesImgs.forEach((img) => {
-  img.addEventListener("click", (e) => {
-    let popupOverlay = document.createElement("div");
-    popupOverlay.className = "popup-overlay";
-    document.body.appendChild(popupOverlay);
-    let popupWindow = document.createElement("div");
-    popupWindow.className = "popup-window";
-    popupOverlay.appendChild(popupWindow);
-    if (img.alt !== null) {
-      let popupHead = document.createElement("h3");
-      let headTextPopup = document.createTextNode(img.alt);
-      popupHead.appendChild(headTextPopup);
-      popupHead.className = "popup-head";
-      popupWindow.appendChild(popupHead);
-    }
-    let popupImg = document.createElement("img");
-    popupImg.className = "popup-img";
-    popupWindow.appendChild(popupImg);
-    popupImg.src = e.target.src;
-    let closePopup = document.createElement("span");
-    let closePopupText = document.createTextNode("X");
-    closePopup.className = "close-popup";
-    closePopup.appendChild(closePopupText);
-    popupWindow.appendChild(closePopup);
-    offThePopup();
-  });
-});
-// Another Way To Close The Popup
-// document.addEventListener("click", (e) => {
-//   if (e.target == document.querySelector(".close-popup")) {
-//     e.target.parentElement.remove();
-//     document.querySelector(".popup-overlay").remove();
-//   }
-// });
-function offThePopup() {
-  document.querySelector(".popup-overlay").onclick = () => {
-    document.querySelector(".popup-overlay").remove();
-  };
-}
+
 // Nav Bullets
 const navBullets = document.querySelectorAll(".nav-bullets .bullet");
 const allLinks = document.querySelectorAll(".header .links");
@@ -263,10 +196,77 @@ window.addEventListener("scroll", (e) => {
     scrollToTop.classList.remove("show");
   }
 });
-
 scrollToTop.addEventListener("click", (e) => {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 });
+// Statics Interval
+let staticSection = document.querySelector(".statics");
+let staticNums = document.querySelectorAll(".static-box .information .number");
+let staticPlus = document.querySelectorAll(
+  ".static-box .information .number + span"
+);
+let startedGo = true;
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY >= staticSection.offsetTop - 800) {
+    if (startedGo) {
+      staticNums.forEach((num) => {
+        setTimeout(() => {
+          staticPlus.forEach((p) => (p.style.opacity = "1"));
+        }, 2000);
+        let staticInt = setInterval(() => {
+          num.innerHTML++;
+          if (num.innerHTML === num.dataset.value) {
+            clearInterval(staticInt);
+          }
+        }, 2000 / num.dataset.value);
+      });
+    }
+    startedGo = false;
+  }
+});
+// Categories Images
+let categoriesImgs = document.querySelectorAll(
+  ".categories-section .categories img"
+);
+categoriesImgs.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    let popupOverlay = document.createElement("div");
+    popupOverlay.className = "popup-overlay";
+    document.body.appendChild(popupOverlay);
+    let popupWindow = document.createElement("div");
+    popupWindow.className = "popup-window";
+    popupOverlay.appendChild(popupWindow);
+    if (img.alt !== null) {
+      let popupHead = document.createElement("h3");
+      let headTextPopup = document.createTextNode(img.alt);
+      popupHead.appendChild(headTextPopup);
+      popupHead.className = "popup-head";
+      popupWindow.appendChild(popupHead);
+    }
+    let popupImg = document.createElement("img");
+    popupImg.className = "popup-img";
+    popupWindow.appendChild(popupImg);
+    popupImg.src = e.target.src;
+    let closePopup = document.createElement("span");
+    let closePopupText = document.createTextNode("X");
+    closePopup.className = "close-popup";
+    closePopup.appendChild(closePopupText);
+    popupWindow.appendChild(closePopup);
+    offThePopup();
+  });
+});
+// Another Way To Close The Popup
+// document.addEventListener("click", (e) => {
+//   if (e.target == document.querySelector(".close-popup")) {
+//     e.target.parentElement.remove();
+//     document.querySelector(".popup-overlay").remove();
+//   }
+// });
+function offThePopup() {
+  document.querySelector(".popup-overlay").onclick = () => {
+    document.querySelector(".popup-overlay").remove();
+  };
+}
